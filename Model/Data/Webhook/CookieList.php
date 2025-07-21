4479ea1712fb64cb3d3d30da1934a4c2fcd80036
Andrii Chaikovskyi <chaikovskyia@ukr.net>
2025-07-21 22:53:26 +0300

    Extended cookie list

---
+++ Model/Data/Webhook/CookieList.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Stape\Gtm\Model\Data\Webhook;
+
+class CookieList
+{
+    public function getAll()
+    {
+        return [
+            '_fbc',
+            '_fbp',
+            'FPGCLAW',
+            '_gcl_aw',
+            'ttclid',
+            '_dcid',
+            'FPID',
+            'FPLC',
+            '_ttp',
+            'FPGCLGB',
+            'li_fat_id',
+            'taboola_cid',
+            'outbrain_cid',
+            'impact_cid',
+            '_epik',
+            '_scid',
+            '_scclid',
+            '_uetmsclkid',
+            '_ga',
+            'FPAU',
+            '_gcl_au',
+            '_gcl_gb',
+            '_gcl_gs',
+            '_gcl_dc',
+            'FPGCLGS',
+            'FPGCLDC',
+            'FPGSID',
+            'awin_awc',
+            'awin_source',
+            'rakuten_site_id',
+            'rakuten_time_entered',
+            'rakuten_ran_mid',
+            'rakuten_ran_eaid',
+            'rakuten_ran_site_id',
+            'cje',
+            'stape_klaviyo_kx',
+            'stape_klaviyo_email',
+            'Stape',
+        ];
+    }
+}
+
